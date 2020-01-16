3f3e10475563594c37e25ccc8e19a8d09018efbd
Keith Potempa <keithrpotempa@gmail.com>
2020-01-16 11:33:40 -0600

    completed MVP

---
+++ main.js
@@ -0,0 +1,24 @@
+/* [x] Use JavaScript to 
+obtain a reference to the first section 
+with the class of article_header 
+and change its text with the textContent property 
+to "Welcome to the {insert your name here} blog" */
+
+let articleHeader = document.querySelector(".article__header");
+articleHeader.textContent = "Welcome to the DOM Blog";
+
+/* [x] Use JavaScript to obtain a reference to all article__header elements
+and change their classList property value to article__header important. */
+
+let allArticleHeaders = document.querySelectorAll(".article__header");
+articleHeader.classList.add("important");
+
+/* [x] Obtain a reference the element with a class of dashed and remove it. */
+let dashed = document.querySelector(".dashed");
+dashed.classList.remove("dashed");
+
+
+/* [x] Obtain a reference the element with a class of article_footer 
+and add the class of goldenrod it. */
+let articleFooter = document.querySelector(".article__footer");
+articleFooter.classList.add("goldenrod");
